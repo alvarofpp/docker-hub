a734d693b640832777bd6527fa11df338733bc83
Álvaro F.P.P <alvarofepipa@gmail.com>
2023-07-01 17:20:36 -0300

    refactor(linter): Updates lint-commit to use origin/main

--- base/linter/commands/lint-commit.sh
+++ base/linter/commands/lint-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TARGET_BRANCH=${1:-"origin/master"}
+TARGET_BRANCH=${1:-"origin/main"}
 TARGET_HEAD_COMMIT=$(git log "${TARGET_BRANCH}" --oneline --pretty=%H | head -1)
 
 commitlint --config="${LINT_CONFIG_PATH}"/.commitlintrc --from="${TARGET_HEAD_COMMIT}"
